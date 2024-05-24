eccffbd573b396f30335fa7d5b348e671cd64055
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-25 00:11:18 +0600

    user schema and interfaces created

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interfaces';
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChanges: {
+      type: Boolean,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'student', 'faculty'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model('User', userSchema);
