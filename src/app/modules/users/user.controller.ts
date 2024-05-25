fa5e080fc34e3066d36c0c36f4139f9e24cdba03
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-26 01:21:59 +0600

    catchAsynch funaction sec and handel try catch

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,34 +1,35 @@
+/* eslint-disable no-unused-vars */
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { userServices } from './user.services';
 import sendResponse from '../../utilis/sendrespons';
 import httpStatus from 'http-status';
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { password, student: studentData } = req.body;
-
-    const result = await userServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is created succesfully',
-    //   data: result,
-    // });
-
-    //function generate response
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: studentData } = req.body;
+
+  const result = await userServices.createStudentIntoDB(password, studentData);
+
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is created succesfully',
+  //   data: result,
+  // });
+
+  //function generate response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created succesfully',
+    data: result,
+  });
+});
 
 export const userControllers = {
   createStudent,
