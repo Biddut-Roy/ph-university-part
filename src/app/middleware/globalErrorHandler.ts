0611355418c04547c0a2159a77dd539821406626
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-06-04 15:51:57 +0600

    handel cast error and 11000 error complete

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -8,6 +8,8 @@ import { TErrorSources } from '../interfaces/error';
 import config from '../config';
 import handelZodError from '../errors/handelZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleDuplicateError from '../errors/handelDuplicateError';
+import handleCastError from '../errors/handelCastError';
 
 const globalErrorHandler = (
   err: any,
@@ -32,6 +34,16 @@ const globalErrorHandler = (
     errorSources = simpleFieldError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
