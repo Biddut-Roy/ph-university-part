3b365af1b39fafb03f5d698d19418ce659552b4b
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-06-05 18:32:20 +0600

    cources all route controler and services complete

---
+++ src/app/modules/course/course.route.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { CourseValidations } from './course.validation';
+import validationRequest from '../../utilis/validationRequest';
+import { CourseControllers } from './course.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-course',
+  validationRequest(CourseValidations.createCourseValidationSchema),
+  CourseControllers.createCourse,
+);
+
+router.get('/:id', CourseControllers.getSingleCourse);
+
+router.patch(
+  '/:id',
+  validationRequest(CourseValidations.updateCourseValidationSchema),
+  CourseControllers.updateCourse,
+);
+
+router.delete('/:id', CourseControllers.deleteCourse);
+
+router.put(
+  '/:courseId/assign-faculties',
+  validationRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.assignFacultiesWithCourse,
+);
+
+router.delete(
+  '/:courseId/remove-faculties',
+  validationRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.removeFacultiesFromCourse,
+);
+
+router.get('/', CourseControllers.getAllCourses);
+
+export const CourseRoutes = router;
